166d4b1215dd2f639ad9a8a5b9e96fbfae4e8d0f
Afoking112 <adewoleafolabi90@gamil.com>
2025-01-15 08:46:27 +0100

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputvalue1,setinputvalue1] = useState("");
+  const [inputvalue2,setinputvalue2] = useState("");
+  // const [displayinput,setdisplayinput] = useState("");
+  // const [displayinput1,setdisplayinput1] = useState("");
+  const [array,setarray] = useState([])
+
+  const inputtype1 = (e)=>{
+    setinputvalue1(e.target.value)
+  }
+  const inputtype2 = (e)=>{
+    setinputvalue2(e.target.value)
+  }
+  const type = ()=>{
+    const newinput =[...array, inputvalue1 ,inputvalue2]
+setarray(newinput)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={inputvalue1} onChange={inputtype1}  />
+      <input value={inputvalue2} onChange={inputtype2}  />
+      <button onClick={type}>click</button>
+      
+      
+        <>
+        {
+          array.map((input,index)=>(
+          
+            <div className='display' key={index}>{input}</div>
+         
+       ))
+        }
+      
+        </>
+    
     </div>
   );
 }
